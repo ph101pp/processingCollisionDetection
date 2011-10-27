1a3fbaba55d5f464e0dab4579de8c5824a9019d1
Philipp C. Adrian <philippcadrian@greenish.ch>
2011-10-27 13:38:46 +0200

    Lorenz JS script anpassung um Werte "on the fly" ändern zu können.

--- deanm-pre3d-5453b65/demos/lorenz84.js
+++ deanm-pre3d-5453b65/demos/lorenz84.js
@@ -3,8 +3,18 @@
 // Inspired by http://www.unitzeroone.com/labs/alchemyPushingPixels/
 // Also reading 'Strange Attractors: Creating Patterns in Chaos'.
 // This attractor is Lorenz-84, with parameters from chaoscope.
+var refresh=function() {
+	var inputs=document.getElementsByName('input');
+	console.log("inputs",inputs);
+	
+	console.log(inputs.length);
+	
+	for(key in inputs) if(key!="item" && key!="length"){
+		inputs[key].addEventListener("change",refresh, false);
+	}
 
-window.addEventListener('load', function() {
+
+
   var screen_canvas = document.getElementById('canvas');
   var renderer = new Pre3d.Renderer(screen_canvas);
 
@@ -13,11 +23,22 @@ window.addEventListener('load', function() {
   var x = 1;
   var y = 1;
   var z = 1;
-
+  
+	var values = [
+		parseFloat(document.getElementById('one').value) || 0,
+		parseFloat(document.getElementById('two').value) || 0,
+		parseFloat(document.getElementById('three').value) || 0,
+		parseFloat(document.getElementById('four').value) || 0,
+		parseFloat(document.getElementById('five').value) || 0,
+		parseFloat(document.getElementById('six').value) || 0,
+		parseFloat(document.getElementById('seven').value) || 0,
+		parseFloat(document.getElementById('eight').value) || 0,
+		parseFloat(document.getElementById('nine').value) || 0
+	];
   function step() {
-    var dx = (-1.111 * x - y * y - z * z + 1.111 * 4.494) * 0.136;
-    var dy = (-y + x * y - 1.479 * x * z + 0.44) * 0.136;
-    var dz = (-z + 1.479 * x * y + x * z) * 0.136;
+    var dx = (values[0] * x - y * y - z * z + values[1] * values[2]) * values[3];
+    var dy = (-y + x * y - values[4] * x * z + values[5]) * values[6];
+    var dz = (-z + values[7] * x * y + x * z) * values[8];
 
     x += dx;
     y += dy;
@@ -58,8 +79,8 @@ window.addEventListener('load', function() {
     {n: 'y', c: new Pre3d.RGBA(1, 1, 0, 1)},
     {n: 'w', c: new Pre3d.RGBA(1, 1, 1, 1)}
   ];
-
-  var fgcolor = new Pre3d.RGBA(1, 0, 0, 1);
+ 
+  var fgcolor = new Pre3d.RGBA(1, 1, 1, 1);
 
   function draw() {
     renderer.ctx.setFillColor(0, 0, 0, 1);
@@ -85,10 +106,13 @@ window.addEventListener('load', function() {
     a.appendChild(document.createTextNode(cm.n));
     colordiv.appendChild(a);
   }
-  document.body.insertBefore(colordiv, document.body.firstChild);
+//  document.body.insertBefore(colordiv, document.body.firstChild);
 
   renderer.camera.focal_length = 2.5;
   DemoUtils.autoCamera(renderer, 0, 0, -15, 0, -1.7, 0, draw);
 
   draw();
-}, false);
+}; 
+window.addEventListener('load', refresh, false);
+
+
