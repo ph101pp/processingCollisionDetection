a5fc8d853fb7063f5674610589601ff791683286
Philipp C. Adrian <philippcadrian@greenish.ch>
2011-12-26 00:59:42 +0100

    some more tests

--- testLibrary/library/testLibrary/testLibrary/testLibrary.java
+++ testLibrary/library/testLibrary/testLibrary/testLibrary.java
@@ -3,19 +3,15 @@ import processing.core.*;
 
 public class testLibrary {
   PApplet parent;
-  
-  PVector hello=new PVector(0,0,0);
 
   public testLibrary(PApplet parent) {
     this.parent = parent;
     parent.registerDraw(this);
     
     parent.println("Hallo");
-    
-    
   }
 
   public void draw() {
-	parent.println(hello);
+	parent.println("library OUTPUT");
 	}
 }
